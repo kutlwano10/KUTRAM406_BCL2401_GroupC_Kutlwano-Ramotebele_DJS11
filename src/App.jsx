8ec4555016fda6a1e8f0661a76d57bb0a048e5eb
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-06-12 14:29:51 +0200

    Initialized react dependencies into the Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./pages/Home";
+import Layout from "./components/Layout";
+
+export const App = () => {
+  return (
+    <>
+      <Layout />
+    </>
+  );
+};
+
+export default App;
