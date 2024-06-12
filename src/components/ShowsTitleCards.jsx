24bc623742cf2701ae4c44db01c6a3c824e5f04d
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-06-13 00:23:46 +0200

    Displayed Shows Cards on Home page
    - Used overflow-x to make shows to scroll

--- src/components/ShowsTitleCards.jsx
+++ src/components/ShowsTitleCards.jsx
@@ -4,17 +4,27 @@ import { useState, useEffect } from "react";
 const ShowsTitleCards = () => {
   const [shows, setShows] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     fetch("https://podcast-api.netlify.app")
-    .then(res => res.json())
-    .then(data => setShows(data))
-    .catch((error) => console.error("Fetch error:" + error))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setShows(data))
+      .catch((error) => console.error("Fetch error:" + error));
+  }, []);
+
+  /**
+   * Maps out shows imgs and titles along with there ids
+   */
+  const showsCards = shows.map((show) => (
+    <div className="card" key={show.id}>
+      <img src={show.image} alt="" />
+      <p>{show.title}</p>
+    </div>
+  ));
 
   return (
     <div className="title-cards">
       <h2>Popular Shows</h2>
-      <div className="card-list"></div>
+      <div className="card-list">{showsCards}</div>
     </div>
   );
 };
