dca742bc5f0409929248064841629673d2d39b14
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-06-17 22:52:01 +0200

    Maped out the sessions using distructuring of usePrams on the Api
    - Added conditional rendering to check if show is null before accessing its properties
    - Display loading message while fetching data
    - Render seasons only if show.seasons exists and is not empty

--- src/pages/PodcastDetails.jsx
+++ src/pages/PodcastDetails.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { useParams  } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const PodcastDetails = () => {
+  const [show, setShow] = useState(null);
   const { id } = useParams();
   useEffect(() => {
     const fetchData = async () => {
@@ -10,17 +11,43 @@ const PodcastDetails = () => {
         const res = await fetch(`https://podcast-api.netlify.app/id/${id}`);
         const data = await res.json();
         console.log(data);
-        setShows(data);
+        setShow(data);
       } catch (error) {
         console.error("Fetch error:" + error);
       }
     };
     fetchData();
-  }, []);
-    
+  }, [id]);
+
+  if (!show) {
+    return <div className="loading"><h1>Loading ...</h1></div>;
+  }
+
   return (
-    <div>
-      <h1>Hello this is details</h1>
+    <div className="podcast-details-container">
+      <h1>Podcast</h1>
+      <div >
+        <h2>{show.title}</h2>
+        <div className="podcast-details">
+          <img src={show.image} alt={show.title} />
+          <p>{show.description}</p>
+        </div>
+      </div>
+      <h2>Seasons</h2>
+      {/* Displaying Podcast seasons */}
+
+      {show.seasons && show.seasons.length > 0 && (
+        <div className="podcast-seasons-container">
+          
+          {show.seasons.map((season, index) => (
+            <div className="podcast-seasons" key={index}>
+              <h3>{season.title}</h3>
+              <img src={season.image} alt={season.title} />
+              <p>{season.description}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
